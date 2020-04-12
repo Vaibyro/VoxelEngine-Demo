830ef211c9304d8c5f53d639f0e311013ade2b2d
Thomas Capodano <t.capodano@gmail.com>
2020-04-13 01:57:18 +0200

    Tasks first version (gpu compute shaders bug)

--- Assets/Scripts/Generators/SurfaceNetsGenerator.cs
+++ Assets/Scripts/Generators/SurfaceNetsGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
@@ -7,7 +8,6 @@ using Debug = UnityEngine.Debug;
 
 namespace VoxelEngine {
     public class SurfaceNetsGenerator : VoxelGenerator {
-        
         // Buffers
         private readonly List<Vector3> _verticesBuffer = new List<Vector3>();
         private readonly List<int> _trianglesBuffer = new List<int>();
@@ -19,21 +19,19 @@ namespace VoxelEngine {
 
             // PART 2 - Create vertices
             var (vertices, vertIndices) = GetVertices(densityGrid);
-            
+
             // PART 3 - Create faces
-            var triangles = GetTriangles(vertices, vertIndices, densityGrid);
+            var triangles = GetTriangles(vertIndices, densityGrid);
 
             return new MeshData(vertices, triangles);
         }
-        
+
         private (List<Vector3>, Dictionary<Vector3Int, int>) GetVertices(DensityData densityData) {
-             _verticesBuffer.Clear();
-             _verticesIndicesBuffer.Clear();
-            
-             
-            for (var xi = 0; xi < gridSize.x - 1; xi++) {
-                for (var yi = 0; yi < gridSize.y - 1; yi++) {
-                    for (var zi = 0; zi < gridSize.z - 1; zi++) {
+            var verticesDictionary = new ConcurrentDictionary<Vector3Int, Vector3>();
+
+            Parallel.For(0, gridSize.x - 1, xi => {
+                Parallel.For(0, gridSize.y - 1, yi => {
+                    Parallel.For(0, gridSize.z - 1, zi => {
                         var vertexPos = Vector3.zero;
                         var counter = 0;
 
@@ -48,13 +46,14 @@ namespace VoxelEngine {
                                         Adapt(densityData[xi + dx, yi + dy, zi + 0],
                                             densityData[xi + dx, yi + dy, zi + 1])
                                     );
-                                    
+
                                     change = Vector3.Scale(change, UnitVector);
                                     vertexPos += change;
                                     counter++;
                                 }
                             }
                         }
+
 
                         for (var dx = 0; dx <= 1; dx++) {
                             for (var dz = 0; dz <= 1; dz++) {
@@ -85,7 +84,7 @@ namespace VoxelEngine {
                                             dy,
                                             dz
                                         );
-                                    
+
                                     change = Vector3.Scale(change, UnitVector);
                                     vertexPos += change;
                                     counter++;
@@ -95,25 +94,31 @@ namespace VoxelEngine {
 
                         if (counter != 0) {
                             vertexPos /= (float) counter;
-                            _verticesIndicesBuffer.Add(new Vector3Int(xi, yi, zi), _verticesBuffer.Count);
-                            _verticesBuffer.Add(vertexPos + GetDensityPosition(xi, yi, zi));
+                            verticesDictionary.TryAdd(new Vector3Int(xi, yi, zi),
+                                vertexPos + GetDensityPosition(xi, yi, zi));
                         }
-                    }
-                }
+                    });
+                });
+            });
+
+            _verticesBuffer.Clear();
+            _verticesIndicesBuffer.Clear();
+
+            foreach (var vertice in verticesDictionary) {
+                _verticesIndicesBuffer.Add(vertice.Key, _verticesBuffer.Count);
+                _verticesBuffer.Add(vertice.Value);
             }
 
             return (_verticesBuffer, _verticesIndicesBuffer);
         }
-         
-        private List<int> GetTriangles(IEnumerable<Vector3> vertices, Dictionary<Vector3Int, int> indexes, DensityData densityData) {
+
+        private List<int> GetTriangles(Dictionary<Vector3Int, int> indexes, DensityData densityData) {
             _trianglesBuffer.Clear();
-            
+
             for (var xi = 0; xi < gridSize.x - 1; xi++) {
                 for (var yi = 0; yi < gridSize.y - 1; yi++) {
                     for (var zi = 0; zi < gridSize.z - 1; zi++) {
                         if (xi > 0 && yi > 0) {
-                            //var solid1 = density.Get(position + GetDensityPosition(xi, yi, zi + 0)) > 0f;
-                            //var solid2 = density.Get(position + GetDensityPosition(xi, yi, zi + 1)) > 0f;
                             var solid1 = densityData[xi, yi, zi + 0] > 0f;
                             var solid2 = densityData[xi, yi, zi + 1] > 0f;
                             if (solid1 != solid2) {
@@ -138,8 +143,6 @@ namespace VoxelEngine {
                         }
 
                         if (xi > 0 && zi > 0) {
-                            //var solid1 = density.Get(position + GetDensityPosition(xi, yi + 0, zi)) > 0f;
-                            //var solid2 = density.Get(position + GetDensityPosition(xi, yi + 1, zi)) > 0f;
                             var solid1 = densityData[xi, yi + 0, zi] > 0f;
                             var solid2 = densityData[xi, yi + 1, zi] > 0f;
                             if (solid1 != solid2) {
@@ -166,11 +169,9 @@ namespace VoxelEngine {
                         }
 
                         if (yi > 0 && zi > 0) {
-                            //var solid1 = density.Get(position + GetDensityPosition(xi + 0, yi, zi)) > 0f;
-                            //var solid2 = density.Get(position + GetDensityPosition(xi + 1, yi, zi)) > 0f;
                             var solid1 = densityData[xi + 0, yi, zi] > 0f;
                             var solid2 = densityData[xi + 1, yi, zi] > 0f;
-                            
+
                             if (solid1 != solid2) {
                                 if (solid2) {
                                     //t1
---
+++ Assets/Scripts/MeshData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VoxelEngine {
+    public class MeshData {
+        public List<Vector3> vertices;
+        public List<int> triangles;
+
+        public MeshData(List<Vector3> vertices, List<int> triangles) {
+            this.vertices = vertices;
+            this.triangles = triangles;
+        }
+    }
+}
